6e225b2acb81ae487460fec6516b89e3b85cb7e8
lokusuke <suzloku63@gmail.com>
2026-04-04 23:59:02 +0900

    HTML追加・JS追加

--- js/index.js
+++ js/index.js
@@ -1,1 +1,44 @@
 "use strict";
+
+// キャラクター表示部分のHTMLElement
+const characterListElement = document.getElementById("character-list");
+
+// ラジオボタン部分の要素を取得し配列に格納
+const characterCategoryElementList = document.querySelectorAll(
+  '[name="character-category"]', // name="character-category"のタグを持つ要素を配列に入れる
+);
+
+// debug
+console.log(characterCategoryElementList);
+
+// 同じname属性のラジオボタンのどれかが押されたことをトリガーに実行される関数の定義
+characterCategoryElementList.forEach((element) => {
+  element.addEventListener("change", (event) => {
+    if (event.target.name != "character-category") {
+      return;
+    }
+
+    // debug
+    console.log(event);
+    console.log(event.target.value);
+
+    switch (event.target.value) {
+      case "demon-hunters":
+        console.log("鬼殺隊を表示する");
+        break;
+      case "pillars":
+        console.log("柱を表示する");
+        break;
+      case "demons":
+        console.log("鬼を表示する");
+        break;
+      default:
+        console.log("全キャラクターを表示する");
+    }
+  });
+});
+
+//Todo
+// 各キャラクターを表示する処理を書く
+
+// 鬼殺隊
